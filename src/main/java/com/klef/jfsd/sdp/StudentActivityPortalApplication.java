b50f318ef96c173b38ad9dce161dc0c88b55a440
Devaseesh Kumar Sonti <devaseesh.sonti2005@gmail.com>
2024-12-08 06:40:36 +0530

    Fix-1

--- src/main/java/com/klef/jfsd/sdp/StudentActivityPortalApplication.java
+++ src/main/java/com/klef/jfsd/sdp/StudentActivityPortalApplication.java
@@ -2,8 +2,10 @@ package com.klef.jfsd.sdp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.klef.jfsd.sdp")
 public class StudentActivityPortalApplication {
 
 	public static void main(String[] args) {
