380263faaf56cc00a91b0769063320767cec218f
Sonti Devaseesh Kumar <147408494+DevaseeshKumar@users.noreply.github.com>
2024-12-10 10:35:08 +0530

    Update AdminController.java

--- src/main/java/com/klef/jfsd/sdp/controller/AdminController.java
+++ src/main/java/com/klef/jfsd/sdp/controller/AdminController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.klef.jfsd.sdp.model.Admin;
@@ -17,6 +18,7 @@ import com.klef.jfsd.sdp.service.AdminService;
 
 import jakarta.servlet.http.HttpServletRequest;
 
+@RequestMapping("/")
 @Controller
 public class AdminController {
 	
