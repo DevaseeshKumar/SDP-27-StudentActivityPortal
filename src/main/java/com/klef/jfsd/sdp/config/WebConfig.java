6633f1e087046f83552c35c6eae03a3094415f74
Sonti Devaseesh Kumar <147408494+DevaseeshKumar@users.noreply.github.com>
2024-12-10 11:09:04 +0530

    Created WebConfig.java

---
+++ src/main/java/com/klef/jfsd/sdp/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.klef.jfsd.sdp.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
