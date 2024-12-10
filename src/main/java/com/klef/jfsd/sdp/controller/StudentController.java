660c43847caeaabfc6103474df7409385425352f
Sonti Devaseesh Kumar <147408494+DevaseeshKumar@users.noreply.github.com>
2024-12-10 10:35:05 +0530

    Update StudentController.java

--- src/main/java/com/klef/jfsd/sdp/controller/StudentController.java
+++ src/main/java/com/klef/jfsd/sdp/controller/StudentController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -17,6 +18,7 @@ import com.klef.jfsd.sdp.service.StudentService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
+@RequestMapping("/")
 @Controller
 public class StudentController {
 
